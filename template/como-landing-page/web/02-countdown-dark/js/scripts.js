66975702e79234d5d2e5e0f8810c77b49badb376
assafshp <assaf.shp@gmail.com>
2020-06-09 15:57:14 +0300

    initial commit

---
+++ template/como-landing-page/web/02-countdown-dark/js/scripts.js
@@ -0,0 +1,91 @@
+/* Template: Como - Coming Soon Landing Page Template
+   Author: Inovatik
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict"; 
+	
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+    });
+
+
+    /* Countdown Timer - The Final Countdown */
+	$('#clock').countdown('2020/12/27 08:50:56') /* change here your "countdown to" date */
+	.on('update.countdown', function(event) {
+		var format = '<span class="counter-number">%D<br><span class="timer-text">Days</span></span><span class="counter-number">%H<br><span class="timer-text">Hours</span></span><span class="counter-number">%M<br><span class="timer-text">Minutes</span></span><span class="counter-number">%S<br><span class="timer-text">Seconds</span></span>';
+		$(this).html(event.strftime(format));
+	})
+	.on('finish.countdown', function(event) {
+	$(this).html('This offer has expired!')
+		.parent().addClass('disabled');
+    });
+
+
+    /* Sign Up Form */
+    $("#signUpForm").validator().on("submit", function(event) {
+    	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            sformError();
+            ssubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            ssubmitForm();
+        }
+    });
+
+    function ssubmitForm() {
+        // initiate variables with form content
+		var email = $("#semail").val();
+        $.ajax({
+            type: "POST",
+            url: "php/signupform-process.php",
+            data: "email=" + email, 
+            success: function(text) {
+                if (text == "success") {
+                    sformSuccess();
+                } else {
+                    sformError();
+                    ssubmitMSG(false, text);
+                }
+            }
+        });
+	}
+
+    function sformSuccess() {
+        $("#signUpForm")[0].reset();
+        ssubmitMSG(true, "Signed Up!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function sformError() {
+        $("#signUpForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass();
+        });
+	}
+
+    function ssubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#smsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
